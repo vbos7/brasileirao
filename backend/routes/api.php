e41d4fb560efe97e5e6b351c3c552c7f93df80e4
Vinícius Boschetti <chagas.vb@gmail.com>
2026-03-17 17:33:31 -0300

    feat: add register controller and test for user registration

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::post('/register', RegisterController::class);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
